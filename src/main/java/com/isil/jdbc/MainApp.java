1a99fade992925212ec6fcb68d5281690626d16a
unknown <luis1078.sebas3059@gmail.com>
2022-09-10 18:54:48 -0500

    Segunda sesion para mantenimiento

--- src/main/java/com/isil/jdbc/MainApp.java
+++ src/main/java/com/isil/jdbc/MainApp.java
@@ -1,9 +1,8 @@
 package com.isil.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import javax.print.attribute.ResolutionSyntax;
+import javax.xml.transform.Result;
+import java.sql.*;
 
 public class MainApp {
     public static void main (String[] args  )throws Exception
@@ -16,14 +15,39 @@ public class MainApp {
 
         Statement st = con.createStatement();
 
-        ResultSet rs = st.executeQuery("select * from Users");
+         // int filasAfectadas = st.executeUpdate("insert into Users values(5,'Juan','984444459', 'Vancouver')");
 
-        while (rs.next()){
-            System.out.println(rs.getString( "idUser") + " " +
-                                rs.getString("name") + " " +
-                                rs.getString( "phone") + " " +
-                                rs.getString("city") + " " + "\n");
+        //int filasAfectadas = st.executeUpdate("update Users set name='Christian' where name = 'Cueva'");
+
+        //int filasAfectadas = st.executeUpdate("delete from Users where idUser=4");
+        //System.out.println("Filas afectadas: " + filasAfectadas);
+
+
+        PreparedStatement preparedStatement = con.prepareStatement("insert into Users values(?,?,?,?)");
+        preparedStatement.setInt(1,4);
+        preparedStatement.setString(2,"Marta");
+        preparedStatement.setString(3,"98782912");
+        preparedStatement.setString(4,"Bogota");
+        int filasAfectadas = preparedStatement.executeUpdate();
+        System.out.println("Filas afectadas: " + filasAfectadas);
+
+
+        PreparedStatement preparedStatement2 = con.prepareStatement("select * FROM Users WHERE idUser=?");
+        preparedStatement2.setInt(1,4);
+
+        ResultSet rs = preparedStatement2.executeQuery();
+
+        //ResultSet rs = st.executeQuery("select * from Users");
+
+            while (rs.next()){
+           System.out.println(rs.getString( "idUser") + " " +
+                               rs.getString("name") + " " +
+                              rs.getString( "phone") + " " +
+                             rs.getString("city") + " " + "\n");
 
         }
+
+
     }
+
 }
